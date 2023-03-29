09c12dd26d54baa69032170704a71243b134577e
kishanth12 <kishanth1209@gmail.com>
2023-03-29 21:45:30 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,32 @@
+
+  
+  function myFunction(){
+  let text = "Hello";
+  const myArr = text.split("");
+  console.log(myArr)
+  const ages = [18,2,73,55];
+
+  let age=0;
+  for(let i=0;i<ages.length; i++){
+    age +=ages[i]
+  }
+  console.log(age)
+
+  const heding = "learning javascript";
+  const person = "kishan";
+ // const fullSentence =person +" "+ " is " + heding +" from "
+  const fullSentence = `${person} is ${heding} from srilanka`
+  console.log(fullSentence)
+
+  }
+  myFunction()
+
+
+
+
+
+  
+
+  
+
+  
